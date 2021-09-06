710b0dc45e73815c843c3f44061a83fe71653edd
mazo7474 <ismail.abdirahman@student.moringaschool.com>
2021-09-06 11:21:43 +0300

    add names

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
-//add inputs fields
-const centuryInput = document.querySelector("[name='century']");
-const yearInput = document.querySelector("[name'year']");
-const monthInput = document.querySelector("[name:'month']");
-const dayInput = document.querySelector("[name'date']");
-const gendersForm = document.querySelector("form#genderform");
-const submitButton = document.getElementById("submit")
 
+//add inputs fields
+const akanName = document.querySelector("Akan-name")
+const centuryInput = document.querySelector("[name ='century']");
+const yearInput = document.querySelector("[name ='year']");
+const monthInput = document.querySelector("[name ='month']");
+const dayInput = document.querySelector("[name ='date']");
+const genderInput = document.querySelector("[name = 'gender']");
+const submitButton = document.getElementById("submit")
 //add onChange event listeners
-centuryInput.addEventListener(
-    "change",
+centuryInput.querySelector(
+    "formValue",
     (e) =>(FormData.century = e.target.value)
 );
-yearInput.addEventListener("change", (e) => (FormData.year = e.target.value));
-monthInput.addEventListener(
-    "change",
+yearInput.querySelector("formValue", (e) => (FormData.year = e.target.value));
+monthInput.querySelector(
+    "formValue",
     (e) =>(FormData.month = e.target.value)
 );
-dayInput.addEventListener("change", (e) => (FormData.day= e.target.value));
-gendersForm.addEventListener(
-    "change",
+dayInput.querySelector("formValue", (e) => (FormData.day= e.target.value));
+genderInput.querySelector(
+    "formValue",
     (e) => (FormData.gender =e.target.value)
 );
 
-submitButton.addEventListener("click", () => {
+submitButton.querySelector("formValue", () => {
     console.log(FormData);
     console.log(getDayOfTheWeek(FormData));
 });
@@ -34,9 +35,18 @@ const mName = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 function getDayOfTheWeek(data) {
     return newDate(data.century, data.month, data.day).getDayOfTheWeek();
 }
+if (date) {
+    if (document.getElementById("male").checked) {
+    akanName.textContent =`Your name in akan is ${mName[date]}.`;
+    }
+    if (document.getElementById("female").checked) {
+    akanName.textContent =`Your name in akan is ${fName[date]}.`;
+
+}};
 
 
 
 
 
- 
+
+
